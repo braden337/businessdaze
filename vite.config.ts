27598ddc55f6424d07fd007d24011d8f36279f5d
Braden Jonsson <braden337@gmail.com>
2022-04-09 12:54:41 -0500

    add test coverage

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,5 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  test: {},
+  test: { watch: false },
 });
