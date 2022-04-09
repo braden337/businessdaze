8bc42b1f1411d60f897b2fb2194dc51352cd55df
Braden Jonsson <braden337@gmail.com>
2022-04-09 14:28:14 -0500

    rebrand and deploy

---
+++ src/lib/BusinessDays.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import { BusinessDaysInMonth } from './date';
+
+  const businessDaysInMonth = new BusinessDaysInMonth();
+
+  let chosen = Date.now();
+
+  $: day = new Date(chosen);
+
+  $: businessDays = businessDaysInMonth.calculate(day);
+</script>
+
+<div>
+  <header>📈🤑💰</header>
+  <h1>{businessDays} business daze</h1>
+  <label for="date">
+    Choose Month:
+    <input type="date" bind:value={chosen} id="date" />
+  </label>
+</div>
+
+<style>
+  div {
+    display: grid;
+    grid-gap: 1rem;
+  }
+
+  header {
+    font-size: 4rem;
+  }
+</style>
